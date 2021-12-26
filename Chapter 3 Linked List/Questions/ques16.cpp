cac26acf0ce45e79c476c93d7a19b9af659aab50
MANRAJ SINGH <manrajmannhere@gmail.com>
2021-12-26 13:41:42 +0530

    Updated

--- Chapter 3 Linked List/Questions/ques16.cpp
+++
@@ -1,5 +0,0 @@
-// Problem-41 Given a linked list consists of data, a next pointer and also a random pointer
-// which points to a random node of the list. Give an algorithm for cloning the list.
-
-
-// not clear question 
